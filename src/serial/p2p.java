0fed3a841142fccf41dd02ac00cc09e1b5c0e5d9
chung-edison <eddy2809@hotmail.com>
2016-12-07 23:04:12 -0500

    Serverside receive and save file

--- src/serial/p2p.java
+++ src/serial/p2p.java
@@ -2,6 +2,7 @@ package serial;
 
 import java.awt.List;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.ObjectInput;
 import java.io.ObjectInputStream;
@@ -17,83 +18,84 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class p2p {
-	
-public static void main(String args[]){
-	startServer();
-    startClient();
+
+	public static void main(String args[]) {
+		startServer();
+		startClient();
+	}
+
+	private static void startClient() {
+		(new Thread() {
+			@Override
+			public void run() {
+
+				try {
+					// Create a socket
+					Socket soc = new Socket(InetAddress.getLocalHost(), 60020);
+					OutputStream o = soc.getOutputStream();
+					ObjectOutput s = new ObjectOutputStream(o);
+					listarCarpeta list = new listarCarpeta();
+					list.setDir(new File("./carpetaCompartida"));
+
+					if (list.ficheros == null) {
+						System.out.println("No hay ficheros en el directorio especificado");
+					} else {
+						for (int x = 0; x < list.ficheros.length; x++)
+							System.out.println(list.ficheros[x]);
+					}
+
+					Chunk test = new Chunk();
+					Path path = Paths.get(filepath());
+					byte[] data = Files.readAllBytes(path);
+					test.setInfo(data);
+					test.setNombre(fileName());
+					test.setId(0);
+					s.writeObject(test);
+					s.flush();
+					s.close();
+
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+					System.out.println("Error during serialization");
+					System.exit(1);
+				}
+			}
+		}).start();
+	}
+
+	private static void startServer() {
+
+		(new Thread() {
+			@Override
+			public void run() {
+
+				ServerSocket ser = null;
+				Socket soc = null;
+				Chunk d = null;
+
+				try {
+					ser = new ServerSocket(60020);
+					/*
+					 * This will wait for a connection to be made to this
+					 * socket.
+					 */
+					soc = ser.accept();
+					InputStream o = soc.getInputStream();
+					ObjectInput s = new ObjectInputStream(o);
+					d = (Chunk) s.readObject();
+					s.close();
+
+					FileOutputStream fos = new FileOutputStream(d.getNombre());
+					fos.write(d.getInfo());
+					fos.close();
+
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+					System.out.println("Error during serialization");
+					System.exit(1);
+
+				}
+			}
+		}).start();
+	}
 }
-
-
-private static void startClient() {
-	(new Thread(){
-		@Override
-		public void run(){
-				
-    try {
-      // Create a socket
-      Socket soc = new Socket(InetAddress.getLocalHost(), 60020);
-      OutputStream o = soc.getOutputStream();
-      ObjectOutput s = new ObjectOutputStream(o);
-      listarCarpeta list = new listarCarpeta();
-      list.setDir(new File("./carpetaCompartida"));
-      
-      if (list.ficheros == null){
-      	  System.out.println("No hay ficheros en el directorio especificado");
-    	}else { 
-      	  for (int x=0;x<list.ficheros.length;x++)
-      	    System.out.println(list.ficheros[x]);
-      	}
-      
-      Chunk test = new Chunk();
-	  Path path = Paths.get(filepath());
-	  byte[] data = Files.readAllBytes(path);
-	  test.setInfo(data);
-	  test.setNombre(fileName());
-	  test.setId(0);
-      s.writeObject(test);
-      s.flush();
-      s.close();
-  
-    } catch (Exception e) {
-      System.out.println(e.getMessage());
-      System.out.println("Error during serialization");
-      System.exit(1);
-  }
-		}
-	}).start();
-}
-
-
-private static void startServer() {
-	
-	(new Thread(){
-		@Override
-		public void run(){
-	ServerSocket ser = null;
-    Socket soc = null;
-    String str = null;
-    ArrayList<String> d = null;
-
-    try {
-      ser = new ServerSocket(60020);
-      System.out.println("server started");
-      /*
-       * This will wait for a connection to be made to this socket.
-       */
-      soc = ser.accept();
-      InputStream o = soc.getInputStream();
-      ObjectInput s = new ObjectInputStream(o);
-      System.out.println("file received");
-      s.close();
-      
-    } catch (Exception e) {
-        System.out.println(e.getMessage());
-        System.out.println("Error during serialization");
-        System.exit(1);
-    }
-		}
-	}).start();
-}
-}
-
-
