25935d9a9c133eef909b47c11dd3b1afd609b8e6
chung-edison <eddy2809@hotmail.com>
2016-12-08 00:41:36 -0500

    Receive multiple files test branch

--- src/serial/p2p.java
+++ src/serial/p2p.java
@@ -1,6 +1,7 @@
 package serial;
 
 import java.awt.List;
+import java.io.EOFException;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
@@ -20,121 +21,135 @@ import java.util.Arrays;
 import java.util.Collections;
 
 public class p2p {
-	
-public static void main(String args[]){
-	startServer();
-    startClient();
-}
 
-
-private static void startClient() {
-	(new Thread(){
-	
-		@Override
-		public void run(){
-    try {
-      // Create a socket
-      Socket soc = new Socket(InetAddress.getLocalHost(), 60020);
-      OutputStream o = soc.getOutputStream();
-      ObjectOutput s = new ObjectOutputStream(o);
-      File carpeta = new File("./carpetaCompartida");
-      Chunk test = new Chunk();
-      File archivo=null;
-	  
-      if (!carpeta.exists()) { 
-    	 
-    	  carpeta.mkdirs();
-    	  System.out.println("se creo la carpeta compartida");
-      
-      }else{
-    	  System.out.println("la carpeta ya existe");
-      }
-      
-      String[] ficheros=carpeta.list();
-      String[] newFicheros;
-      for (int i = 0; i < ficheros.length; i++) {
-  		System.out.println(ficheros[i]);
-  	}
-      while(true){
-    	  newFicheros=carpeta.list();
-    	  if(ficheros.length!=newFicheros.length){
-    		  break;
-    	  }
-      }
-      System.out.println("salio del while");
-      ArrayList ficherosArr = new ArrayList();
-      ArrayList newFicherosArr = new ArrayList();
-      Collections.addAll(ficherosArr, ficheros);
-      Collections.addAll(newFicherosArr, newFicheros);
-      newFicherosArr.removeAll(ficherosArr);
-      for (int i = 0; i < newFicherosArr.size(); i++) {
-		
-    	System.out.println(newFicherosArr.get(i));
-		archivo= new File("./carpetaCompartida/"+newFicherosArr.get(i));
-		Path path = Paths.get("./carpetaCompartida/"+newFicherosArr.get(i));
-		byte[] data = Files.readAllBytes(path);
-		test.setInfo(data);
-		test.setNombre(String.valueOf(newFicherosArr.get(i)));
-		test.setId(i);
-	    s.writeObject(test);
+	public static void main(String args[]) {
+		startServer();
+		startClient();
 	}
-      
-      
-      s.flush();
-      s.close();
-  
-    } catch (Exception e) {
-      System.out.println(e.getMessage());
-      System.out.println("Error during serialization");
-      System.exit(1);
-  }
-		}
-	}).start();
-}
 
+	private static void startClient() {
+		(new Thread() {
 
-private static void startServer() {
-	
-	(new Thread() {
-		@Override
-		public void run() {
+			@Override
+			public void run() {
+				try {
 
-			ServerSocket ser = null;
-			Socket soc = null;
-			Chunk d = null;
+					ListaIPs iplist = new ListaIPs();
 
-			try {
-				ser = new ServerSocket(60020);
-				/*
-				 * This will wait for a connection to be made to this
-				 * socket.
-				 */
-				soc = ser.accept();
-				InputStream o = soc.getInputStream();
-				ObjectInput s = new ObjectInputStream(o);
-				d = (Chunk) s.readObject();
-				s.close();
-				File carpeta= new File("./carpetaRec");
-				if (!carpeta.exists()) { 
-			    	  carpeta.mkdirs();
-			    	  System.out.println("se creo la carpeta compartida Rec");
-			      
-			      }else{
-			    	  System.out.println("la carpeta Rec ya existe");
-			      }
-				FileOutputStream fos = new FileOutputStream("./carpetaRec/"+d.getNombre());
-				fos.write(d.getInfo());
-				fos.close();
+					File carpeta = new File("./carpetaCompartida");
+					Chunk test = new Chunk();
+					File archivo = null;
 
-			} catch (Exception e) {
-				System.out.println(e.getMessage());
-				System.out.println("Error during serialization");
-				System.exit(1);
+					if (!carpeta.exists()) {
 
+						carpeta.mkdirs();
+						System.out.println("se creo la carpeta compartida");
+
+					} else {
+						System.out.println("la carpeta ya existe");
+					}
+
+					String[] ficheros = carpeta.list();
+					String[] newFicheros;
+					for (int i = 0; i < ficheros.length; i++) {
+						System.out.println(ficheros[i]);
+					}
+					while (true) {
+
+						newFicheros = carpeta.list();
+						if (ficheros.length != newFicheros.length) {
+
+							Socket soc = new Socket(iplist.getIplist().get(0), 60020);
+							OutputStream o = soc.getOutputStream();
+							ObjectOutput s = new ObjectOutputStream(o);
+
+							System.out.println("Se detectó un cambio en la carpeta: sincronizando...");
+							ArrayList<String> ficherosArr = new ArrayList<String>();
+							ArrayList<String> newFicherosArr = new ArrayList<String>();
+							Collections.addAll(ficherosArr, ficheros);
+							Collections.addAll(newFicherosArr, newFicheros);
+							newFicherosArr.removeAll(ficherosArr);
+							for (String fichero : newFicherosArr) {
+
+								System.out.println(fichero);
+
+								archivo = new File("./carpetaCompartida/" + fichero);
+								Path path = Paths.get("./carpetaCompartida/" + fichero);
+
+								byte[] data = Files.readAllBytes(path);
+
+								test.setInfo(data);
+								test.setNombre(String.valueOf(fichero));
+								test.setId(newFicherosArr.indexOf(fichero));
+
+								s.writeObject(test);
+								s.flush();
+
+							}
+							ficheros = carpeta.list();
+							System.out.println("Sincronización exitosa.");
+							s.close();
+						}
+
+						try {
+							Thread.sleep(1000); // 1000 milliseconds
+						} catch (InterruptedException ex) {
+							Thread.currentThread().interrupt();
+						}
+					}
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+					System.out.println("Error during serialization");
+					System.exit(1);
+				}
 			}
-		}
-}).start();
-}
-}
+		}).start();
+	}
 
+	private static void startServer() {
 
+		(new Thread() {
+			@Override
+			public void run() {
+
+				ServerSocket ser = null;
+				Socket soc = null;
+				Chunk d = null;
+
+				try {
+					ser = new ServerSocket(60020);
+					/*
+					 * This will wait for a connection to be made to this
+					 * socket.
+					 */
+					soc = ser.accept();
+					InputStream o = soc.getInputStream();
+					ObjectInput s = new ObjectInputStream(o);
+
+					File carpeta = new File("./carpetaRec");
+					if (!carpeta.exists()) {
+						carpeta.mkdirs();
+						System.out.println("se creo la carpeta compartida Rec");
+					} else {
+						System.out.println("la carpeta Rec ya existe");
+					}
+					while (s.available() != 0) {
+						d = (Chunk) s.readObject();
+						FileOutputStream fos = new FileOutputStream("./carpetaRec/" + d.getNombre());
+						fos.write(d.getInfo());
+						fos.close();
+						System.out.println(d.getNombre());
+					}
+
+					s.close();
+
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+					System.out.println("Error during serialization");
+					System.exit(1);
+
+				}
+			}
+		}).start();
+	}
+}
